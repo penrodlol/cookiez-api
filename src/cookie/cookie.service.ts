868e411ae6986de60d3e5b8d27f8bf4f3d39ea28
penrodlol <penrod.christian7984@gmail.com>
2020-12-20 16:42:54 -0500

    orderby update desc

--- src/cookie/cookie.service.ts
+++ src/cookie/cookie.service.ts
@@ -14,6 +14,7 @@ export class CookieService {
   async findAll() {
     return this.model
       .find()
+      .sort({ updated: 'desc' })
       .exec();
   }
   
--- src/type/type.service.ts
+++ src/type/type.service.ts
@@ -14,6 +14,7 @@ export class TypeService {
   async findAll() {
     return this.model
       .find()
+      .sort({ updated: 'desc' })
       .exec();
   }
   
